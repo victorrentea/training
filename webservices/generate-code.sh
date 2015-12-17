c2a9385169da9e3175f4a8d8e6e46e782849cf49
VictorRentea <victorrentea@gmail.com>
2015-12-17 13:26:06 +0200

    webservices for linux

--- webservices/generate-code.sh
+++ webservices/generate-code.sh
@@ -1,12 +1,12 @@
-rm -rf src/main/gen-java
-mkdir src/main/gen-java
+rm -rf src/main/gen
+mkdir src/main/gen
 
 #Bare generation
-#wsimport -target 2.2 -s src/main/gen-java -Xnocompile src/main/resources/wsdl/responsibility.v1.wsdl
+#wsimport -target 2.2 -s src/main/gen -Xnocompile src/main/resources/wsdl/responsibility.v1.wsdl
 
 #with binding for WSDL
-#wsimport -target 2.2 -s src/main/gen-java -b src/main/resources/wsdl/responsibility.v1.wsdl.bindings.xml -Xnocompile src/main/resources/wsdl/responsibility.v1.wsdl
+#wsimport -target 2.2 -s src/main/gen -b src/main/resources/wsdl/responsibility.v1.wsdl.bindings.xml -Xnocompile src/main/resources/wsdl/responsibility.v1.wsdl
 
 
 #with binding for WSDL and XSD
-wsimport -target 2.2 -s src/main/gen-java -b src/main/resources/wsdl/responsibility.v1.wsdl.bindings.xml -b src/main/resources/wsdl/responsibility.v1.xsd.bindings.xml -Xnocompile src/main/resources/wsdl/responsibility.v1.wsdl
+wsimport -target 2.2 -s src/main/gen -b src/main/resources/wsdl/responsibility.v1.wsdl.bindings.xml -b src/main/resources/wsdl/responsibility.v1.xsd.bindings.xml -Xnocompile src/main/resources/wsdl/responsibility.v1.wsdl
