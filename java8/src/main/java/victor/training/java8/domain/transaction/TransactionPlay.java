e64a8a28e5d207db6b82ed9991aa955aca97d91c
VictorRentea <victorrentea@gmail.com>
2016-04-18 17:19:43 +0300

    with ABS

--- java8/src/main/java/victor/training/java8/domain/transaction/TransactionPlay.java
+++ java8/src/main/java/victor/training/java8/domain/transaction/TransactionPlay.java
@@ -1,16 +1,11 @@
 package victor.training.java8.domain.transaction;
 
 import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.partitioningBy;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class TransactionPlay {
@@ -54,96 +49,60 @@ public class TransactionPlay {
 		);
 		
 		
-		System.out.println(transactions.stream()
+		// 1
+		transactions.stream()	
 			.filter(t -> t.getYear() == 2011)
 			.sorted(Comparator.comparing(Transaction::getValue))
-			.collect(toList()));
-		
-		System.out.println(transactions.stream()
-				.map(Transaction::getTrader)
-				.anyMatch(t->"Milan".equals(t.getCity())));
-
-		transactions.stream()
-			.filter(t->t.getTrader().getCity().equals("Cambridge"))
-			.map(Transaction::getValue)
 			.collect(toList());
 		
+		// 2
 		transactions.stream()
-			.mapToInt(Transaction::getValue)
-			.max();
-		
-		
-//		select angajat
-//		from angajati
-//		unde angajat.salauri = (select max salariud from )
-		
-		int minValue = transactions.stream()
-				.mapToInt(Transaction::getValue)
-				.min()
-				.getAsInt();
-		
-		Transaction tr2 = transactions.stream()
-			.filter(tr->tr.getValue() == minValue)
-			.findAny()
-			.get();
-		
-		
-//		1 1 2 3 5 8 
-		
-		Stream<Integer> fibonacci = Stream
-				.iterate(new int[]{1,1}, (int[] prev) -> new int[]{prev[1], prev[0] + prev[1]})
-				.map(pair -> pair[0])
-				;
-		
-		fibonacci.limit(1000)
-			.forEach(System.out::println);;
-		
-		
-		int N = 50;
-		
-		Predicate<Integer> isPrime = x->true;
-		
-		 Map<Boolean, List<Integer>> result = IntStream.rangeClosed(1, N).boxed()
-			.collect(partitioningBy(isPrime));
-			
-		Predicate<User> under50bis = user -> user.getAge() < 50;
-		 
-		Function<Integer, Predicate<User>> ageUnder = age -> user -> user.getAge() < age;
-		Predicate<User> under50 = ageUnder.apply(50);
-		Predicate<User> under40 = ageUnder.apply(40);
-		
-		System.exit(2);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+			.map(Transaction::getTrader)
+			.map(Trader::getCity)
+			.distinct()
+			.collect(toList());
+	
+		// 3
 		transactions.stream()
-			.filter(t->t.getYear() == 2011)
-			.sorted(Comparator.comparing(Transaction::getValue))
+			.map(Transaction::getTrader)
+			.filter(trader -> "Cambridge".equals(trader.getCity()))
+			.sorted(Comparator.comparing(Trader::getName))
 			.collect(toList());
 		
-		System.out.println(transactions.stream()
+		// 4
+		String joined = transactions.stream()
 			.map(Transaction::getTrader)
 			.map(Trader::getName)
-			.distinct()
 			.sorted()
-			.collect(joining(",")));
+			.distinct()
+			.collect(joining(":"));
+		System.out.println(joined);
+			
+		// 5
+		boolean b = transactions.stream()
+			.map(Transaction::getTrader)
+			.anyMatch(trader -> "Milano".equals(trader.getCity()));
 		
-		System.out.println(transactions.stream().map(Transaction::getTrader).anyMatch(t -> t.getCity().equals("Milan")));
-		
+		// 6
 		transactions.stream()
-			.filter(t -> t.getTrader().getCity().equals("Cambridge"))
-			.map(Transaction::getValue)
-			.forEach(System.out::println);
-		System.out.println(transactions.stream()
-			.mapToInt(Transaction::getValue)
-			.max()
-			.getAsInt());
+			.filter(t-> "Cambridge".equals(t.getTrader().getCity()))
+			.mapToInt(Transaction::getValue) //  returns IntStream
+			.sum();
+		
+		
+		// 7
+		int ttt = transactions.stream()
+			.max(Comparator.comparing(Transaction::getValue))
+			.get()
+			.getValue();
+		
+//		int val = 1_000_000;
+		
+		Stream<Integer> fibonacci = Stream.iterate(
+				new int[]{1,1}, 
+				pair -> new int[]{pair[1], pair[0]+pair[1]})
+				.map(pair -> pair[0]);
+		
+		fibonacci.limit(10).forEach(System.out::println);
 	}
 }
