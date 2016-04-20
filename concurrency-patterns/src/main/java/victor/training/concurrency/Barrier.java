71dacf8a9b9d592a23151081a30adf177bf57600
VictorRentea <victorrentea@gmail.com>
2016-04-20 17:20:11 +0300

    victorrentea.ro + concurrency add ABS

--- concurrency-patterns/src/main/java/victor/training/concurrency/Barrier.java
+++ concurrency-patterns/src/main/java/victor/training/concurrency/Barrier.java
@@ -1,16 +1,17 @@
 package victor.training.concurrency;
 
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
-
 import static victor.training.concurrency.ConcurrencyUtil.log;
 import static victor.training.concurrency.ConcurrencyUtil.sleepSomeTime;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class Barrier {
-	
+	public static final int NO_PARTICIPANTS = 3;
+	public static final int NO_DAYS = 5;
 	// static CyclicBarrier barrier = null; // TODO // INITIAL
 	// SOLUTION(
-	static CyclicBarrier barrier = new CyclicBarrier(3, new Runnable() {
+	static CyclicBarrier barrier = new CyclicBarrier(NO_PARTICIPANTS, new Runnable() {
 		int count;
 		@Override
 		public void run() {
@@ -21,10 +22,10 @@ public class Barrier {
 	// SOLUTION)
 	
 	public static void main(String[] args) {
-		for (int i=0;i<3;i++) {
+		for (int i=0;i<NO_PARTICIPANTS;i++) {
 			new Thread() {
 				public void run() {
-					for (int i=0; i< 4; i++) {
+					for (int i=0; i < NO_DAYS; i++) {
 						sleepSomeTime(500, 1000);
 						log("Waiting for everyone else to start the stand-up");
 						// TODO .await at the barrier
