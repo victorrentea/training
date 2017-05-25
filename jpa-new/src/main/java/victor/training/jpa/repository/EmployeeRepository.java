3d0cc83811aaff136165e0e5701d8911d6519795
Victor Rentea <victorrentea@gmail.com>
2017-05-25 16:46:52 +0300

    jpa new : Stream<EmployeeExporVO>

--- jpa-new/src/main/java/victor/training/jpa/repository/EmployeeRepository.java
+++ jpa-new/src/main/java/victor/training/jpa/repository/EmployeeRepository.java
@@ -2,12 +2,14 @@ package victor.training.jpa.repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import victor.training.jpa.entity.Employee;
+import victor.training.jpa.entity.EmployeeExportVO;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>, EmployeeRepositoryCustom {
@@ -20,6 +22,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer>, Em
 	public Optional<Employee> getByName(Integer employeeId);
 	
 	public Long countByName(String name);
+	
+	@Query("SELECT new victor.training.jpa.entity.EmployeeExportVO(e.id, e.name, e.site.name) FROM Employee e")
+	Stream<EmployeeExportVO> getUsersForExport();
 	
 	// TODO extends my own super common interface (take from Core)
 }
