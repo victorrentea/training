118fbcae16702daa7db8c00920430290aa40675a
Victor Rentea <victorrentea@gmail.com>
2017-06-07 02:40:06 +0300

    test

--- clean-lambdas/src/main/java/victor/clean/lambdas/TypeSpecificFunctionality.java
+++ clean-lambdas/src/main/java/victor/clean/lambdas/TypeSpecificFunctionality.java
@@ -17,7 +17,9 @@ class Movie {
 	@Getter @Setter private Type type;
 	
 	public double computePrice(int daysRented) {
+		
 		switch (type) {
+		
 		case CHILDREN: return 5;
 		case REGULAR: return 8 + 0.3 * (daysRented - 8);
 		case NEW_RELEASE: return 10 + 0.5 * daysRented;
