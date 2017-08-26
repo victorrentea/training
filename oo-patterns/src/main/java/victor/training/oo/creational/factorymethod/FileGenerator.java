71c95cd76bdc8eaf05715f65c7fd92b8800d3e17
Victor Rentea <victorrentea@gmail.com>
2017-08-27 00:27:26 +0300

    OO DP  Builder

--- oo-patterns/src/main/java/victor/training/oo/creational/factorymethod/FileGenerator.java
+++ oo-patterns/src/main/java/victor/training/oo/creational/factorymethod/FileGenerator.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class FileGenerator {
 	public File createAnafFile() {
 //		OutputFile outputFile = new OutputFile("anaf", '|'); // INITIAL
-		OutputFile outputFile = OutputFile.timestampedCsv("anaf", '|'); // SOLUTION
+		OutputFile outputFile = OutputFile.aCsv("anaf", '|'); // SOLUTION
 		
 		File file = new File(outputFile.getFileName());
 		System.out.println("Writing ANAF to file " + file.getAbsolutePath());
@@ -14,8 +14,8 @@ public class FileGenerator {
 	}
 	
 	public File createCreditBureauFile(int month) {
-//		OutputFile outputFile = new OutputFile("credit-"+month, false); // INITIAL
-		OutputFile outputFile = OutputFile.csvWithSemicolon("credit-"+month, false); // SOLUTION
+//		OutputFile outputFile = new OutputFile("credit-"+month); // INITIAL
+		OutputFile outputFile = OutputFile.csvWithSemicolon("credit-"+month); // SOLUTION
 		
 		File file = new File(outputFile.getFileName());
 		System.out.println("Writing Credit to file " + file.getAbsolutePath());
@@ -30,7 +30,7 @@ public class FileGenerator {
 		
 //		System.out.println(new OutputFile("bank", "txt").getFileName()); // INITIAL
 //		System.out.println(new OutputFile("countries").getFileName()); // INITIAL
-		System.out.println(OutputFile.timestampedWithSemicolon("bank", "txt").getFileName()); // SOLUTION
-		System.out.println(OutputFile.timestampedCsvWithSemicolon("countries").getFileName()); // SOLUTION
+		System.out.println(OutputFile.withSemicolon("bank", "txt").getFileName()); // SOLUTION
+		System.out.println(OutputFile.csvWithSemicolon("countries").getFileName()); // SOLUTION
 	}
 }
--- oo-patterns/src/main/java/victor/training/oo/creational/factorymethod/OutputFile.java
+++ oo-patterns/src/main/java/victor/training/oo/creational/factorymethod/OutputFile.java
@@ -1,60 +1,44 @@
 package victor.training.oo.creational.factorymethod;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class OutputFile {
 	private final String baseName;
 	private final String extension;
 	private final char separator;
-	private final boolean appendTimestamp;
 	
-	public OutputFile(String baseName, String extension, char separator, boolean appendTimestamp) {
+	public OutputFile(String baseName, String extension, char separator) {
 		this.baseName = baseName;
 		this.extension = extension;
 		this.separator = separator;
-		this.appendTimestamp = appendTimestamp;
 	}
 	
 //	public OutputFile(String baseName) { // INITIAL(
-//		this(baseName, "csv", ';', true);
+//		this(baseName, "csv", ';');
 //	} // INITIAL)
-	public static OutputFile timestampedCsvWithSemicolon(String baseName) { // SOLUTION(
-		return new OutputFile(baseName, "csv", ';', true);
+	public static OutputFile csvWithSemicolon(String baseName) { // SOLUTION(
+		return new OutputFile(baseName, "csv", ';');
 	} // SOLUTION)
 	
 	
 //	public OutputFile(String baseName, String extension) { // INITIAL( 
-//		this(baseName, extension, ';', true);
+//		this(baseName, extension, ';');
 //	} // INITIAL)
-	public static OutputFile timestampedWithSemicolon(String baseName, String extension) { // SOLUTION(
-		return new OutputFile(baseName, extension, ';', true);
+	public static OutputFile withSemicolon(String baseName, String extension) { // SOLUTION(
+		return new OutputFile(baseName, extension, ';');
 	} 
 	// SOLUTION)
 	
 	
 //	public OutputFile(String baseName, char separator) { // INITIAL(
-//		this(baseName, "csv", separator, true);
+//		this(baseName, "csv", separator);
 //	} // INITIAL)
-	public static OutputFile timestampedCsv(String baseName, char separator) { // SOLUTION(
-		return new OutputFile(baseName, "csv", separator, true);
+	public static OutputFile aCsv(String baseName, char separator) { // SOLUTION(
+		return new OutputFile(baseName, "csv", separator);
 	} // SOLUTION)
 	
 	
-	
-//	public OutputFile(String baseName, boolean appendTimestamp) { // INITIAL(
-//		this(baseName, "csv", ';', appendTimestamp);
-//	} // INITIAL)
-	public static OutputFile csvWithSemicolon(String baseName, boolean appendTimestamp) { // SOLUTION(
-		return new OutputFile(baseName, "csv", ';', appendTimestamp);
-	} // SOLUTION)
 	
 	public String getFileName() {
-		String timestamp = "";
-		if (appendTimestamp) {
-			timestamp = "-" + new SimpleDateFormat("yyyyMMdd").format(new Date());
-		}
-		return baseName + timestamp + "." + extension;
+		return baseName + "." + extension;
 	}
 	
 	public char getSeparator() {
