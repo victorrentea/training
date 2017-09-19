7d9691b2ba5771c6a94b564b90eed727290d6d5d
Victor Rentea <victorrentea@gmail.com>
2017-09-19 17:02:12 +0300

    Strategy for TaxCalculator

--- oo-patterns-php/victor/training/oo/behavioral/strategy/CustomsService.php
+++ oo-patterns-php/victor/training/oo/behavioral/strategy/CustomsService.php
@@ -8,25 +8,51 @@
 
 namespace victor\training\oo\behavioral\strategy;
 
+include "TaxCalculator.php";
 
 class CustomsService
 {
 
-    public function computeAddedCustomsTax(string $originCountry, double $tobaccoValue, double $otherValue): double { // UGLY API we CANNOT change
-		switch ($originCountry) {
-		case "UK": return $tobaccoValue/2 + $otherValue/2;
-		case "CH": return $tobaccoValue + $otherValue;
-		case "FR":
-		case "ES": // other EU country codes...
-		case "RO": return $tobaccoValue/3;
-		default: throw new \Exception("Not a valid country ISO2 code: " . $originCountry);
-		}
+    public function computeAddedCustomsTax(string $originCountry, float $tobaccoValue, float $otherValue): float { // UGLY API we CANNOT change
+		return $this->determineTaxCalculator($originCountry)
+            ->computeTax($tobaccoValue, $otherValue);
     }
 
+    private function determineTaxCalculator(string $originCountry): TaxCalculator
+    {
+        switch ($originCountry) {
+            case "UK": return new UKTaxCalculator();
+            case "CH": return new ChinaTaxCalculator();
+            case "FR":
+            case "ES": // other EU country codes...
+            case "RO": return new EUTaxCalculator();
+            default: throw new \Exception("Not a valid country ISO2 code: " . $originCountry);
+        }
+    }
+}
+
+class UKTaxCalculator implements TaxCalculator {
+    function computeTax(float $tobaccoValue, float $otherValue = 0): float
+    {
+        return $tobaccoValue/2 + $otherValue/2;
+    }
+}
+class ChinaTaxCalculator implements TaxCalculator {
+    function computeTax(float $tobaccoValue, float $otherValue = 0): float
+    {
+        return $tobaccoValue + $otherValue;
+    }
+}
+class EUTaxCalculator implements TaxCalculator {
+    function computeTax(float $tobaccoValue, float $otherValue = 0): float
+    {
+        return $tobaccoValue/3;
+    }
 }
 
 
+
 $customsService = new CustomsService();
-printf("Tax for (RO,100,100) = " . $customsService->computeAddedCustomsTax("RO", 100, 100));
-printf("Tax for (CH,100,100) = " . $customsService->computeAddedCustomsTax("CH", 100, 100));
-printf("Tax for (UK,100,100) = " . $customsService->computeAddedCustomsTax("UK", 100, 100));
+printf("Tax for (RO,100,100) = " . $customsService->computeAddedCustomsTax("RO", 100, 100) . "\n");
+printf("Tax for (CH,100,100) = " . $customsService->computeAddedCustomsTax("CH", 100, 100). "\n");
+printf("Tax for (UK,100,100) = " . $customsService->computeAddedCustomsTax("UK", 100, 100). "\n");
---
+++ oo-patterns-php/victor/training/oo/behavioral/strategy/TaxCalculator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: VictorRentea
+ * Date: 9/19/2017
+ * Time: 4:55 PM
+ */
+
+namespace victor\training\oo\behavioral\strategy;
+
+
+interface TaxCalculator
+{
+    function computeTax(float $tobaccoValue, float $otherValue = 0): float;
+}
