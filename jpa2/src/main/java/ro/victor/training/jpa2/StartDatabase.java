302eb0ef75a01452a62bae70dfa4e8ae5f7cb261
Victor Rentea <victorrentea@gmail.com>
2018-03-16 20:19:52 +0200

    jpa2 with H2 (supports REQUIRES_NEW).

--- jpa2/src/main/java/ro/victor/training/jpa2/StartDatabase.java
+++ jpa2/src/main/java/ro/victor/training/jpa2/StartDatabase.java
@@ -1,7 +1,13 @@
 package ro.victor.training.jpa2;
 
+import java.sql.SQLException;
+
 public class StartDatabase {
-	public static void main(String[] args) {
-		org.hsqldb.server.Server.main("--database.0 mem:test --dbname.0 test".split(" "));
+	public static void main(String[] args) throws SQLException {
+		//hsqldb does not support Nested Transactions (REQUIRES_NEW).
+//		org.hsqldb.server.Server.main("--database.0 mem:test --dbname.0 test".split(" "));
+		
+		// H2 does :)
+		org.h2.tools.Server.createTcpServer().start();
 	}
 }
