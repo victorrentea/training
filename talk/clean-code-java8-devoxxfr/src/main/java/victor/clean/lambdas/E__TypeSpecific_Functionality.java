91b94eee84c8fc095c20899c9aecddbb46769b16
Victor Rentea <victorrentea@gmail.com>
2018-04-02 14:02:19 +0300

    clean up

--- talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
+++ talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
@@ -5,9 +5,9 @@ package victor.clean.lambdas;
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-//		System.out.println(new Movie(Type.REGULAR).computePrice(2));
-//		System.out.println(new Movie(Type.NEW_RELEASE).computePrice(2));
-//		System.out.println(new Movie(Type.CHILDREN).computePrice(2));
+//		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
+//		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
+//		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
 		System.out.println("COMMIT now!");
 	}
 }
