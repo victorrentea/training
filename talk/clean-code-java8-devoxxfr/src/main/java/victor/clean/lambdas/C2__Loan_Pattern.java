29bb8705a56b260f56b82b7b81e39e52c27e9f4b
Victor Rentea <victorrentea@gmail.com>
2018-04-02 14:56:28 +0300

    fi

--- talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/C2__Loan_Pattern.java
+++ talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/C2__Loan_Pattern.java
@@ -1,26 +1,4 @@
 package victor.clean.lambdas;
 
-import java.io.IOException;
-import java.io.Writer;
-import java.util.stream.Stream;
 
-import org.jooq.lambda.Unchecked;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-interface OrderRepo extends JpaRepository<Order, Long> { // J'aime Spring Data!
-	Stream<Order> findActiveTrue(); // 1 Mln orders ;)
-}
-
-class OrderExporter {
-	private OrderRepo repo;
-	
-	private String toExportString(Order order) {
-		return "something\n";
-	}
-	
-	public void exportToFile(Writer writer) {
-		repo.findActiveTrue()
-			.map(this::toExportString)
-			.forEach(Unchecked.consumer(writer::write));
-	}
-}
