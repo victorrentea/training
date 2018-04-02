36a05963a4a44491e5acfbbf6c3a5d37338d3a41
Victor Rentea <victorrentea@gmail.com>
2018-04-02 10:08:31 +0300

    before refactor B

--- talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/B__Stream_Wrecks.java
+++ talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/B__Stream_Wrecks.java
@@ -5,14 +5,17 @@ import static java.util.stream.Collectors.summingInt;
 import static java.util.stream.Collectors.toList;
 
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map.Entry;
 
+import lombok.Data;
+
 public class B__Stream_Wrecks {
 	
-	public List<Product> stuff2(List<Order> orders) {
-		List<Long> excludedProductIds = getExcludedProductIds();
+	private ProductRepo productRepo;
+	
+	public List<Product> getFrequentOrderedProducts(List<Order> orders) {
+		List<Long> excludedProductIds = productRepo.getExcludedProductIds();
 		return orders.stream()
 				.filter(o -> o.getCreationDate().isAfter(LocalDate.now().minusYears(1)))
 				.flatMap(o -> o.getOrderLines().stream())
@@ -21,28 +24,32 @@ public class B__Stream_Wrecks {
 				.stream()
 				.filter(e -> e.getValue() >= 10)
 				.map(Entry::getKey)
+				.filter(p -> !p.isDeleted())
 				.filter(p->excludedProductIds.contains(p.getId()))
 				.collect(toList());
-	}
-
-	private List<Long> getExcludedProductIds() {
-		return Arrays.asList(1L);
 	}
 }
 
 
 // -- dummy data model --
+@Data
 class Order {
-	public boolean isNotDelivered() {return false;}
-	public List<OrderLine> getOrderLines() {return null;}
-	public LocalDate getCreationDate() {return null;}
+	private List<OrderLine> orderLines;
+	private LocalDate creationDate;
 }
+
+@Data
 class OrderLine {
-	public Product getProduct() {return null;}
-	public boolean isInStock() {return false;}
-	public int getItemCount() {return 0;}
+	private Product product;
+	private int itemCount;
 }
+
+@Data
 class Product {
-	public boolean isNotHidden() {return false;}
-	public Long getId() {return 0l;}
+	private Long id;
+	private boolean deleted;
+}
+
+interface ProductRepo {
+	List<Long> getExcludedProductIds();
 }
