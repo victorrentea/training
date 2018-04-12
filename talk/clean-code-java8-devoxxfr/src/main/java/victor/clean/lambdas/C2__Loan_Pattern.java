ab53d293c19b29776922456aeac76fa490fff298
Victor Rentea <victorrentea@gmail.com>
2018-04-12 14:21:51 +0300

    Loan pattern

--- talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/C2__Loan_Pattern.java
+++ talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/C2__Loan_Pattern.java
@@ -2,38 +2,13 @@ package victor.clean.lambdas;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 
+import org.jooq.lambda.Unchecked;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-interface OrderRepo extends JpaRepository<Order, Long> { // J'aime Spring Data!
-	Stream<Order> findByActiveTrue(); // 1 Mln orders ;)
-}
-
-class FileExporter {
-	private OrderRepo repo;
-		
-	private String toExportLine(Order order) {
-		return "Order " + order.getId() + "\n";
-	}
-	
-	public File exportFile(String fileName) {
-		File file = new File("export/" + fileName);
-		try (Writer writer = new FileWriter(file)) {
-			writer.write("Header\n");
-			repo.findByActiveTrue()
-				.map(this::toExportLine)
-				.forEach(writer::write);
-			log.debug("Generation done");
-			return file;
-		} catch (Exception e) {
-			log.debug("Coucou!", e); // TERREUR !
-			// TODO send email notification
-			throw e;
-		}
-	}
-}
