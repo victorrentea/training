3b5a9f0b3520bd0184250b5ca516ef20454ce821
Victor Rentea <victorrentea@gmail.com>
2018-04-12 15:34:35 +0300

    extarnalizarea calcului in Price Service

--- talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
+++ talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
@@ -1,41 +1,63 @@
 package victor.clean.lambdas;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import lombok.Data;
+import victor.clean.lambdas.Movie.Type;
+
 class Movie {
 	enum Type {
-		REGULAR {
-			public int computePrice(int days) {
-				return days + 1;
-			}
-		}, NEW_RELEASE {
-			public int computePrice(int days) {
-				return days * 2;
-			}
-		}, CHILDREN {
-			public int computePrice(int days) {
-				return 5;
-			}
-		};
-		public abstract int computePrice(int days);
+		REGULAR ,
+		NEW_RELEASE ,
+		CHILDREN ;
 	}
-	
+
 	private final Type type;
-	
+
 	public Movie(Type type) {
 		this.type = type;
 	}
 
-	public int computePrice(int days) {
-		return type.computePrice(days);
+}
+
+// that silly "2" should be updatable in DB -> we depend on DB
+interface NewReleasePriceRepo {
+	double getFactor();
+}
+
+@Data
+class PriceService {
+	private final NewReleasePriceRepo repo;
+	
+	int computeNewReleasePrice(int days) {
+		return (int) (days * repo.getFactor());
+	}
+	int computeRegularPrice(int days) {
+		return days + 1;
+	}
+	int computeChildrenPrice(int days) {
+		return 5;
+	}
+	public int computePrice(Movie.Type type, int days) {
+		switch (type) {
+		case REGULAR: return computeRegularPrice(days);
+		case NEW_RELEASE:return computeNewReleasePrice(days);
+		case CHILDREN: return computeChildrenPrice(days);
+		default: throw new IllegalArgumentException();
+		}
 	}
 }
 
-
-
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
-		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
-		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
+		NewReleasePriceRepo repo = mock(NewReleasePriceRepo.class);
+		when(repo.getFactor()).thenReturn(2d);
+		PriceService service = new PriceService(repo);
+		
+		System.out.println(service.computePrice(Type.REGULAR, 2));
+		System.out.println(service.computePrice(Type.NEW_RELEASE, 2));
+		System.out.println(service.computePrice(Type.CHILDREN, 2));
 		System.out.println("COMMIT now!");
 	}
 }
