ee8dd69babf0f7e65f81f4270380f2e27963410b
Victor Rentea <victorrentea@gmail.com>
2018-04-12 15:28:47 +0300

    don't uuse EXTENDS !!

--- talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
+++ talk/clean-code-java8-devoxxfr/src/main/java/victor/clean/lambdas/E__TypeSpecific_Functionality.java
@@ -1,32 +1,60 @@
 package victor.clean.lambdas;
 
-class Movie {
-	enum Type {
-		REGULAR, NEW_RELEASE, CHILDREN
+//class Movie {
+//	enum Type {
+//		REGULAR, NEW_RELEASE, CHILDREN
+//	}
+//	
+//	private final Type type;
+//	
+//	public Movie(Type type) {
+//		this.type = type;
+//	}
+//
+//	public int computePrice(int days) {
+//		switch (type) {
+//		case REGULAR: return days + 1;
+//		case NEW_RELEASE: return days * 2;
+//		case CHILDREN: return 5;
+//		default: throw new IllegalArgumentException(type.name());
+//		}
+//	}
+//}
+
+abstract class Movie {
+	public abstract int computePrice(int days);
+}
+class RegularMovie extends Movie {
+	public int computePrice(int days) {
+		return days + 1;
 	}
 	
-	private final Type type;
-	
-	public Movie(Type type) {
-		this.type = type;
-	}
+}
+class NewReleaseMovie extends Movie {
 
 	public int computePrice(int days) {
-		switch (type) {
-		case REGULAR: return days + 1;
-		case NEW_RELEASE: return days * 2;
-		case CHILDREN: return 5;
-		}
-		return 0; // ?!.. Free!! Deducted from 
+		return days  *2;
 	}
+	
 }
+class ChildrenMovie extends Movie {
+
+	public int computePrice(int days) {
+		return 5;
+	}
+	
+}
+
 
 
 public class E__TypeSpecific_Functionality {
 	public static void main(String[] args) {
-		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
-		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
-		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
+//		System.out.println(new Movie(Movie.Type.REGULAR).computePrice(2));
+//		System.out.println(new Movie(Movie.Type.NEW_RELEASE).computePrice(2));
+//		System.out.println(new Movie(Movie.Type.CHILDREN).computePrice(2));
+		System.out.println(new RegularMovie().computePrice(2));
+		System.out.println(new NewReleaseMovie().computePrice(2));
+		System.out.println(new ChildrenMovie().computePrice(2));
 		System.out.println("COMMIT now!");
 	}
 }
