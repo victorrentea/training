09ffc41d0641245b458c0f0cf6e17eb617de02ed
Victor Rentea <victorrentea@gmail.com>
2018-04-27 00:53:40 +0300

    2

---
+++ kata/kata-racing-TelemetrySystem-java/src/main/java/tddmicroexercises/telemetrysystem/TicTacToe.java
@@ -0,0 +1,10 @@
+package tddmicroexercises.telemetrysystem;
+
+public class TicTacToe {
+
+	public static String getBoard() {
+		return "  123\nA|___\nB|___\nC|___";
+	}
+
+
+}
--- kata/kata-racing-TelemetrySystem-java/src/main/java/tddmicroexercises/telemetrysystem/TicTacToeTest.java
+++ kata/kata-racing-TelemetrySystem-java/src/main/java/tddmicroexercises/telemetrysystem/TicTacToeTest.java
@@ -1,5 +1,13 @@
 package tddmicroexercises.telemetrysystem;
 
-public class TicTacToe {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class TicTacToeTest {
+	@Test
+	public void emptyBoard() {
+		assertEquals("  123\nA|___\nB|___\nC|___", TicTacToe.getBoard());
+	}
+	
 }
