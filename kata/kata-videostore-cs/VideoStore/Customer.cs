2581800dc6a0046309c4c0e2466c60e82ab215c8
Victor Rentea <victorrentea@gmail.com>
2019-04-16 09:20:54 +0300

    CS

---
+++ kata/kata-videostore-cs/VideoStore/Customer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace VideoStore
+{
+    public class Customer
+    {
+        private readonly ArrayList _rentals = new ArrayList();
+        public string Name { get; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void AddRental(Rental rental)
+        {
+            _rentals.Add(rental);
+        }
+
+        public string Statement()
+        {
+            decimal totalAmount = 0;
+            var frequentRenterPoints = 0;
+            var result = "Rental Record for " + Name + "\n";
+            foreach (var each in _rentals.Cast<Rental>())
+            {
+                decimal thisAmount = 0;
+
+                switch (each.Movie.PriceCode)
+                {
+                    case Movie.Regular:
+                        thisAmount += 2;
+                        if (each.DaysRented > 2)
+                            thisAmount += (each.DaysRented - 2)*1.5m;
+                        break;
+                    case Movie.NewRelease:
+                        thisAmount += each.DaysRented*3;
+                        break;
+                    case Movie.Childrens:
+                        thisAmount += 1.5m;
+                        if (each.DaysRented > 3)
+                            thisAmount += (each.DaysRented - 3)*1.5m;
+                        break;
+                    default:
+                        break;
+                }
+                // add frequent renter points
+                frequentRenterPoints++;
+                // add bonus for a two day new release rental
+                if ((each.Movie.PriceCode == Movie.NewRelease) &&
+                    each.DaysRented > 1)
+                    frequentRenterPoints++;
+
+                // show figures for this rental
+                result += $"\t{each.Movie.Title}\t"+ $"{thisAmount:F1}\n";
+                totalAmount += thisAmount;
+            }
+
+            // add footer lines
+            result += $"Amount owed is "+ $"{totalAmount:F1}\n";
+            result += $"You earned {frequentRenterPoints} frequent renter points";
+            return result;
+        }
+    }
+}
---
+++ kata/kata-videostore-cs/VideoStoreSpec/VideoStoreSpec.cs
@@ -0,0 +1,30 @@
+﻿using NBehave.Spec.NUnit;
+using NUnit.Framework;
+using Rhino.Mocks.Impl.Invocation.Actions;
+using VideoStore;
+
+namespace VideoStoreSpec
+{
+    [TestFixture]
+    public class VideoStoreSpec 
+    {
+       
+        [Test]
+        public void CharacterizationTest()
+        {
+            Customer customer = new Customer("John Doe");
+            customer.AddRental(new Rental(new Movie("Star Wars", Movie.NewRelease), 6));
+            customer.AddRental(new Rental(new Movie("Sofia", Movie.Childrens), 7));
+            customer.AddRental(new Rental(new Movie("Inception", Movie.Regular), 5));
+
+            string expected = "Rental Record for John Doe\n"
+                    + "	Star Wars	18.0\n"
+                    + "	Sofia	7.5\n"
+                    + "	Inception	6.5\n"
+                    + "Amount owed is 32.0\n"
+                    + "You earned 4 frequent renter points";
+
+            customer.Statement().ShouldEqual(expected);
+        }
+    }
+}
