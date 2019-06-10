2e99e2468c20c86f50c712ed8e793de0380bb060
Victor Rentea <victorrentea@gmail.com>
2019-06-11 01:02:14 +0300

    Init Binder

--- spring-security/gs-securing-web-master/initial/src/main/java/hello/MyController.java
+++ spring-security/gs-securing-web-master/initial/src/main/java/hello/MyController.java
@@ -2,11 +2,16 @@ package hello;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.RequestContext;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Slf4j
 @Controller
@@ -29,5 +34,18 @@ public class MyController {
     @ResponseBody
     public String locale(HttpServletRequest request) {
         return new RequestContext(request).getLocale().toString();
+    }
+
+    @GetMapping("date")
+    @ResponseBody
+    public String convertDate(@RequestParam Date date) {
+        return new SimpleDateFormat("yyyyMMdd").format(date);
+    }
+
+    @InitBinder
+    public void dateBinder(WebDataBinder binder) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        dateFormat.setLenient(false);
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
     }
 }
