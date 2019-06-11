74911d1983c318668246659a29a57dc4fe039476
Victor Rentea <victorrentea@gmail.com>
2019-06-12 00:31:22 +0300

    @SpringBootTest

--- unit-testing/src/test/java/ro/victor/unittest/mvc/MvcTest.java
+++ unit-testing/src/test/java/ro/victor/unittest/mvc/MvcTest.java
@@ -6,34 +6,32 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = MvcApplication.class)
+//@WebAppConfiguration
+//@ContextConfiguration(classes = MvcApplication.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class MvcTest {
 
-	@Autowired
-    private WebApplicationContext wac;
+//	@Autowired
+//    private WebApplicationContext wac;
 
+	@Autowired
     private MockMvc mockMvc;
 
-    @Before
-    public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-    }
+//    @Before
+//    public void setup() {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+//    }
     
     @Test
     public void peaceTest() throws Exception {
