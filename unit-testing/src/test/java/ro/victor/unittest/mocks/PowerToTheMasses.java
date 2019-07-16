b8b3f8a8f910adfa56ae8f69135a5f207539ec59
Victor Rentea <victorrentea@gmail.com>
2019-07-16 23:43:20 +0300

    Fixed PowerMock

--- unit-testing/src/test/java/ro/victor/unittest/mocks/PowerToTheMasses.java
+++ unit-testing/src/test/java/ro/victor/unittest/mocks/PowerToTheMasses.java
@@ -43,3 +43,23 @@ public class PowerToTheMasses {
 	}
 	
 }
+
+class OldSingleton{
+	private static OldSingleton INSTANCE;
+	private OldSingleton() {
+	}
+	public static OldSingleton getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new OldSingleton();
+		}
+//			return INSTANCE;
+		throw new IllegalArgumentException();
+	}
+	private String state;
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+}
