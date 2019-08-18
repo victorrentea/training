add883a92d4e70296cb38573ec2d2a5c4b01aeec
Victor Rentea <victorrentea@gmail.com>
2019-08-18 21:04:29 +0300

    Clear performance

--- performance/src/main/java/victor/perf/leaks/BigObject200KB.java
+++
@@ -1,8 +0,0 @@
-package victor.perf.leaks;
-
-import java.util.Date;
-
-public class BigObject200KB {
-	public Date date = new Date();
-	public int[] largeArray = new int[50*1024];
-}
--- performance/src/main/java/victor/perf/leaks/RequestContext.java
+++
@@ -1,8 +0,0 @@
-package victor.perf.leaks;
-
-import victor.perf.leaks.CachingMethodObject.UserRightsCalculator;
-
-public class RequestContext {
-
-	public UserRightsCalculator rights;
-}
