5c7fc4b7a8ab5009ff58b7d75a1f1521177118c6
Victor Rentea <victorrentea@gmail.com>
2019-09-19 17:16:01 +0300

    revert back solution clean code php

--- kata/kata-videostore-php/src/Movie.php
+++ kata/kata-videostore-php/src/Movie.php
@@ -1,62 +1,57 @@
 <?php
 namespace video;
 
-
-class RegularMovie extends Movie {
-    public function calculatePrice(int $daysRented): float
-    {
-        $price = 2;
-        if ($this->getDaysRented() > 2)
-            $price += ($this->getDaysRented() - 2) * 1.5;
-        return $price;
-    }
-
-}
-//class NewReleaseMovie extends Movie {
-//
-//}
-//class ChildrensMovie extends Movie {
-//
-//}
-class BabaciMovie extends Movie {
-
-    public function calculatePrice(int $daysRented): float
-    {
-        // TODO: Implement calculatePrice() method.
-    }
-}
-
-abstract class Movie
+class Movie
 {
     const NEW_RELEASE = "NEW_RELEASE";
     const REGULAR = "REGULAR";
     const CHILDRENS = "CHILDRENS";
-    const ELDERS = "BABACI";
-
-    public abstract function calculatePrice(int $daysRented): float;
 
     private $title;
 
     private $priceCode;
 
-    public function __construct(string $title, string $priceCode)
+    /**
+     * Movie constructor.
+     * @param string $title
+     * @param string $priceCode
+     */
+    public function __construct($title, $priceCode)
     {
         $this->title = $title;
         $this->priceCode = $priceCode;
     }
 
-    public function getTitle(): string
+    /**
+     * @return mixed
+     */
+    public function getTitle()
     {
         return $this->title;
     }
 
-    public function getPriceCode(): string
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPriceCode()
     {
         return $this->priceCode;
     }
 
-    public function isNewRelease(): bool
+    /**
+     * @param mixed $priceCode
+     */
+    public function setPriceCode($priceCode)
     {
-        return $this->priceCode === self::NEW_RELEASE;
+        $this->priceCode = $priceCode;
     }
+
 }
--- kata/kata-videostore-php/src/Rental.php
+++ kata/kata-videostore-php/src/Rental.php
@@ -3,73 +3,49 @@ namespace video;
 
 class Rental
 {
+    /**
+     * @var Movie
+     */
     private $movie;
     private $daysRented;
 
-    public function __construct(Movie $movie, int $daysRented)
+    /**
+     * Rental constructor.
+     * @param Movie $movie
+     * @param $daysRented
+     */
+    public function __construct(Movie $movie, $daysRented)
     {
         $this->movie = $movie;
         $this->daysRented = $daysRented;
     }
 
-    public function getDaysRented(): int
+
+    /**
+     * @return int
+     */
+    public function getDaysRented()
     {
         return $this->daysRented;
     }
 
-    public function getMovie(): Movie
+    /**
+     * @param int $daysRented
+     */
+    public function setDaysRented($daysRented)
+    {
+        $this->daysRented = $daysRented;
+    }
+
+    /**
+     * @return Movie
+     */
+    public function getMovie()
     {
         return $this->movie;
     }
 
-    public function calculatePrice(): float
-    {
-        switch ($this->movie->getPriceCode()) {
-            case Movie::REGULAR: return $this->calculateRegularPrice();
-            case Movie::NEW_RELEASE: return $this->calculateNewReleasePrice();
-            case Movie::CHILDRENS: return $this->calculateChildrensPrice();
-            case Movie::ELDERS: return $this->calculateChildrensPrice();
-            default: throw new \Exception("Dau bere dac-o prinzi: " . $this->movie->getTitle());
-        }
-    }
-    public function calculateAge(): float
-    {
-        switch ($this->movie->getPriceCode()) {
-            case Movie::REGULAR: return $this->calculateRegularPrice();
-            case Movie::NEW_RELEASE: return $this->calculateNewReleasePrice();
-            case Movie::CHILDRENS: return $this->calculateChildrensPrice();
-            default: throw new \Exception("Dau bere dac-o prinzi: " . $this->movie->getTitle());
-        }
-    }
 
-    public function calculateFrequentRenterPoints(): int
-    {
-        $frequentRenterPoints = 1;
 
-        if ($this->movie->isNewRelease() && $this->getDaysRented() >= 2) {
-            $frequentRenterPoints++;
-        }
-        return $frequentRenterPoints;
-    }
 
-    private function calculateRegularPrice(): float
-    {
-        $price = 2;
-        if ($this->getDaysRented() > 2)
-            $price += ($this->getDaysRented() - 2) * 1.5;
-        return $price;
-    }
-
-    private function calculateNewReleasePrice(): float
-    {
-        return $this->getDaysRented() * 3;
-    }
-
-    private function calculateChildrensPrice(): float
-    {
-        $price = 1.5;
-        if ($this->getDaysRented() > 3)
-            $price += ($this->getDaysRented() - 3) * 1.5;
-        return $price;
-    }
 }
