31ebc2b47daeca4f958b760214a7f8877e605a98
Victor Rentea <victorrentea@gmail.com>
2019-11-04 16:19:50 +0200

    added design patterns C# to clean code kata. YOLO :)

---
+++ kata/kata-videostore-cs/VideoStore/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore
+{
+    class Class1
+    {
+        public static void Main(string[] args)
+        {
+            new EmailService().SendOrderReceivedEmail("a@b.com");
+            Console.ReadLine();
+        }
+    }
+
+    class EmailService
+    {
+
+        public void SendOrderReceivedEmail(String emailAddress)
+        {
+            EmailContext context = new EmailContext(/*smtpConfig,etc*/);
+            int MAX_RETRIES = 3;
+            for (int i = 0; i < MAX_RETRIES; i++)
+            {
+                Email email = new Email(); // constructor generates new unique 
+                email.sender = "noreply@corp.com";
+                email.replyTo = "/dev/null";
+                email.to = emailAddress;
+                email.subject = "Order Received";
+                email.body = "Thank you for your order";
+                bool success = context.send(email);
+                if (success) break;
+            }
+        }
+        class EmailContext
+        {
+            private readonly Random rand = new Random();
+            public bool send(Email email)
+            {
+                Console.Out.WriteLine("Trying to send " + email.asText());
+                return rand.Next() % 2 == 0;
+            }
+        }
+
+        class Email
+        {
+            private readonly int id = new Random().Next();
+            public String subject { get; set; }            
+            public String body;
+            public String sender;
+            public String replyTo;
+            public String to;
+            
+            public string asText()
+            {
+                return $"Email({subject}, {body}, {id})";
+            }
+
+        }
+    }
+  
+}
---
+++ kata/kata-videostore-cs/VideoStore/StrategyApp.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoStore
+{
+    class StrategyApp
+    {
+        public static void Main(string[] args)
+        {
+            CustomsService service = new CustomsService();
+            Console.WriteLine("Tax for (RO,100,100) = " + service.ComputeCustomsTax("RO", 100, 100));
+            Console.WriteLine("Tax for (CN,100,100) = " + service.ComputeCustomsTax("CN", 100, 100));
+            Console.WriteLine("Tax for (UK,100,100) = " + service.ComputeCustomsTax("UK", 100, 100));
+            Console.ReadLine();
+        }
+
+    }
+
+}
+
+class CustomsService
+{
+    
+    public double ComputeCustomsTax(string originCountry, double tobaccoValue, double regularValue)
+    { // UGLY API we CANNOT change
+        ITaxProvider pece = SelectTaxProvider(originCountry);
+        return pece.Get(tobaccoValue, regularValue);
+    }
+
+
+    private  ITaxProvider SelectTaxProvider(string originCountry)
+    {
+        ITaxProvider[] taxProviders = {
+             new EUTaxProvider(),
+            new BrexitTaxProvider() ,
+             new ChinaTaxProvider()};
+        return taxProviders.ToList().First(tp => tp.Accepts(originCountry));
+        //    default: throw new Exception("Not a valid country ISO2 code: " + originCountry);
+        
+    }
+}
+
+interface ITaxProvider
+{
+    bool Accepts(string originCountry);
+    double Get(double tobaccoValue, double regularValue);
+}
+
+class EUTaxProvider : ITaxProvider
+{
+    public bool Accepts(string originCountry)
+    {
+        return new string[] { "FR", "ES", "RO" }.Contains(originCountry);
+    }
+
+    public double Get(double tobaccoValue, double regularValue)
+    {
+        return tobaccoValue / 3;
+    }
+}
+class BrexitTaxProvider : ITaxProvider
+{
+    public bool Accepts(string originCountry)
+    {
+        return originCountry == "UK";
+    }
+
+    public double Get(double tobaccoValue, double regularValue)
+    {
+        return tobaccoValue / 2 + regularValue;
+    }
+}
+
+class ChinaTaxProvider : ITaxProvider
+{
+    public bool Accepts(string originCountry)
+    {
+        return originCountry == "CN";
+    }
+
+    public double Get(double tobaccoValue, double regularValue)
+    {
+  
+        return tobaccoValue + regularValue;
+    }
+}
+
+
