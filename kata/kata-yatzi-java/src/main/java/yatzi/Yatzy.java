9866f6958e66722c9809244792b8a2f87f1989b8
Victor Rentea <victorrentea@gmail.com>
2020-06-26 12:21:25 +0300

    DiceHand + Iterable + Stream.boxed.iterator()

--- kata/kata-yatzi-java/src/main/java/yatzi/Yatzy.java
+++ kata/kata-yatzi-java/src/main/java/yatzi/Yatzy.java
@@ -1,18 +1,35 @@
 package yatzi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+class DiceHand implements Iterable<Integer>{
+   private final int[] dice;
+
+   public DiceHand(int d1, int d2, int d3, int d4, int d5) {
+      this.dice = new int[] {d1, d2, d3, d4, d5};
+   }
+
+   public int[] getDice() {
+      return dice;
+   }
+
+   @Override
+   public Iterator<Integer> iterator() {
+      return IntStream.of(dice).boxed().iterator();
+   }
+}
+
 public class Yatzy {
 
    public static int chance(int d1, int d2, int d3, int d4, int d5) {
-      int total = 0;
-      total += d1;
-      total += d2;
-      total += d3;
-      total += d4;
-      total += d5;
-      return total;
+      return d1 + d2 + d3 + d4 + d5;
    }
 
-   public static int yatzy(int... dice) {
+   public static int yatzy(DiceHand dice) {
       int[] counts = new int[6];
       for (int die : dice)
          counts[die - 1]++;
--- kata/kata-yatzi-java/src/main/java/yatzi/YatzyTest.java
+++ kata/kata-yatzi-java/src/main/java/yatzi/YatzyTest.java
@@ -15,10 +15,11 @@ public class YatzyTest {
 
     @Test public void yatzy_scores_50() {
         int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
-        assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+
+        new DiceHand(1,2,3,4,5).getDice();
+        assertEquals(expected, Yatzy.yatzy(new DiceHand(4,4,4,4,4)));
+        assertEquals(50, Yatzy.yatzy(new DiceHand(6,6,6,6,6)));
+        assertEquals(0, Yatzy.yatzy(new DiceHand(6,6,6,6,3)));
     }
 
     @Test public void test_1s() {
